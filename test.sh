4f8dbaad75de0b320f6afdc3da8a093a1592f188
Alex <alexhutman@gmail.com>
2021-04-30 18:38:10 -0400

    added small test file

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+err_and_exit() {
+	echo "$1" 1>&2
+	exit 1
+}
+if ! gcc b64.c -o b64; then
+	err_and_exit "Compilation failed."
+fi
+
+TEST_STR="hi!"
+
+INTENDED="$(printf $TEST_STR | base64)" 
+RES="$(./b64 test)" 
+
+if [ "$INTENDED" != "$RES" ]; then
+	err_and_exit "Test failed! Received '$RES' instead of '$INTENDED'"
+fi
+
+echo "Test successful!"
