57ab25d14fb25b3b8269abcb6022a8e332a55863
Alex <alexhutman@gmail.com>
2021-05-01 23:04:12 -0400

    works with longer buffers and if they aren't multiples of 3 bytes

--- b64.c
+++ b64.c
@@ -37,31 +37,82 @@ int main(int argc, char *argv[]) {
 void encode_file(FILE *fp) {
 	char tmp = '\0';
 	// For testing, just using a hardcoded buffer
-	// Using size of 3 because 3 = lcm(6,8) / 8
+	// Using a multiple of 3 because 3 = lcm(6,8) / 8
 	// i.e. using 3 chars (octets), we can extract 4 b64 sextets with no wasted space.
-	// I will look into using a multiple of 3 in the future, but it's fine for now in order to test.
-	char in_buf[] = {'h', 'i', '!'};
+	char in_buf[] = {'h', 'i', ' ', 't', 'h', 'e', 'r', 'e', '!'};
+	int buf_size = (int)(sizeof (in_buf))/(sizeof (char));
 	char out_buf[50] = {0};
 
 	// The following representation of the buffer makes it easier to see why these operations are performed
 	// The number represents which sextet the bit is in, the grouping represents each octet
 	// 11111122 | 22223333 | 33444444
 
-	// First sextet
-	tmp = (in_buf[0] >> 2) & 0x3f;
-	out_buf[0] = b64_lookup(tmp);
+	int num_triples = buf_size / 3;
+	int num_extra_bytes = buf_size % 3;
 
-	// Second sextet
-	tmp = (in_buf[0] << 4) & 0x30 | (in_buf[1] >> 4);
-	out_buf[1] = b64_lookup(tmp);
+	int first_oct, second_oct, third_oct;
+	int out_idx = 0;
+	for (int i=0; i<num_triples; i++) {
+		first_oct = i*3;
+		second_oct = first_oct+1;
+		third_oct = first_oct+2;
 
-	// Third sextet
-	tmp = (in_buf[1] << 2) & 0x3c | ((in_buf[2] >> 6) & 0x03);
-	out_buf[2] = b64_lookup(tmp);
+		// First sextet
+		tmp = (in_buf[first_oct] >> 2) & 0x3f;
+		out_buf[out_idx] = b64_lookup(tmp);
+		out_idx++;
 
-	// Fourth sextet
-	tmp = in_buf[2] & 0x3f;
-	out_buf[3] = b64_lookup(tmp);
+		// Second sextet
+		tmp = (in_buf[first_oct] << 4) & 0x30 | (in_buf[second_oct] >> 4);
+		out_buf[out_idx] = b64_lookup(tmp);
+		out_idx++;
+
+		// Third sextet
+		tmp = (in_buf[second_oct] << 2) & 0x3c | ((in_buf[third_oct] >> 6) & 0x03);
+		out_buf[out_idx] = b64_lookup(tmp);
+		out_idx++;
+
+		// Fourth sextet
+		tmp = in_buf[third_oct] & 0x3f;
+		out_buf[out_idx] = b64_lookup(tmp);
+		out_idx++;
+	}
+
+	if (num_extra_bytes > 0) {
+		first_oct += 3;
+		second_oct = first_oct+1;
+		third_oct = first_oct+2;
+
+		// First sextet is the same if we have 1 or 2 extra bytes
+		tmp = (in_buf[first_oct] >> 2) & 0x3f;
+		out_buf[out_idx] = b64_lookup(tmp);
+		out_idx++;
+
+		if (num_extra_bytes == 1) {
+			// Second sextet if only 1 extra byte
+			tmp = (in_buf[first_oct] << 4) & 0x30;
+			out_buf[out_idx] = b64_lookup(tmp);
+			out_idx++;
+
+			// 2 padding characters
+			out_buf[out_idx] = '=';
+			out_buf[out_idx + 1] = '=';
+		}
+		else {
+			// Second sextet if 2 extra bytes
+			tmp = (in_buf[first_oct] << 4) & 0x30 | (in_buf[second_oct] >> 4);
+			out_buf[out_idx] = b64_lookup(tmp);
+			out_idx++;
+
+			// Third sextet if 2 extra bytes
+			tmp = (in_buf[second_oct] << 2) & 0x3c;
+			out_buf[out_idx] = b64_lookup(tmp);
+			out_idx++;
+
+			// 1 padding character
+			out_buf[out_idx] = '=';
+		}
+	}
 
 	printf("%s\n", out_buf);
 }
