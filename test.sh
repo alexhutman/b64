c8e4b9985af26fc06f01dc8a32a9ee4b965100c7
Alex <alexhutman@gmail.com>
2021-05-04 03:20:13 -0400

    fixed tests to reflect reading from input
    also changed interpreter to bash so i can use process substitution

--- test.sh
+++ test.sh
@@ -1,20 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 err_and_exit() {
 	echo "$1" 1>&2
 	exit 1
 }
+
 if ! gcc b64.c -o b64; then
 	err_and_exit "Compilation failed."
 fi
 
-TEST_STR="hi!"
-
+TEST_STR="Hello there, this is a test :)"
 INTENDED="$(printf $TEST_STR | base64)" 
-RES="$(./b64 test.sh)" 
 
-if [ "$INTENDED" != "$RES" ]; then
-	err_and_exit "Test failed! Received '$RES' instead of '$INTENDED'"
+RES="$(printf $TEST_STR | ./b64)" 
+if [ "$INTENDED" = "$RES" ]; then
+	echo "Stdin test succeeded!"
+else
+	err_and_exit "Stdin test failed! Received '$RES' instead of '$INTENDED'"
 fi
 
-echo "Test successful!"
+RES="$(./b64 <(printf $TEST_STR))" 
+if [ "$INTENDED" = "$RES" ]; then
+	echo "File test succeeded!"
+else
+	err_and_exit "File test failed! Received '$RES' instead of '$INTENDED'"
+fi
+
+echo "All tests successful!"
